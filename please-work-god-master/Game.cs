a621b9e9118488a0ceb73e62f23a28367de38fa4
Grace <gnh00004@mix.wvu.edu>
2024-10-19 02:14:49 -0400

    Add files via upload
    Should be correct files for blue square size change instead of color change.

---
+++ please-work-god-master/Game.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Numerics;
+
+namespace Game10003
+{
+    public class Game
+    {
+        private float normalSize = 200f; //variable for normal size of the square
+        private float smallSize; //variable for small size of the square. No number just yet
+
+        public void Setup()
+        {
+            Window.SetTitle("Toggle Size");
+            Window.SetSize(400, 400);
+            smallSize = normalSize / 2; //calculator the smallSize by making it equal to normalSize divided by 2
+        }
+
+
+        public void Update()
+        {
+            Window.ClearBackground(Color.OffWhite);
+
+            //currentSize variable
+            float currentSize;
+
+            //determine if space bare is pushed or not
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Space))
+            {
+                currentSize = smallSize; //set to small size if spacebar is down
+            }
+            else
+            {
+                currentSize = normalSize; //set to normal size if it isnt pressed
+            }
+
+            //set fill color to blue
+            Draw.FillColor = Color.Blue;
+
+            //draws the square with the currentsize
+            Draw.Square(100, 100, currentSize);
+        }
+    }
+}
